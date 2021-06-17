2aef253292a5a9e0498dbe4cc95bfded708907a8
oscar.gallardo@baruchmail.cuny.edu <oscar.gallardo@baruchmail.cuny.edu>
2021-06-17 18:06:21 +0000

    v3

--- models/staging/stripe/stg_payments.sql
+++ models/staging/stripe/stg_payments.sql
@@ -1,17 +1,11 @@
-with payments as (
+select
+    id as payment_id,
+    orderid as order_id,
+    paymentmethod as payment_method,
+    status,
+    -- amount is stored in cents, convert it to dollars
+    amount / 100 as amount,
+    created as created_at
+from {{ source('stripe', 'payment') }}
 
-    select
-        id as payment_id,
-        orderid as order_id,
-        paymentmethod as payment_method,
-        status,
-
-        -- amount is stored in cents, convert it to dollars
-        amount / 100 as amount,
-        created as created_at
-
-    from `dbt-tutorial.stripe.payment`
-
-)
-
-select * from payments
+--select * from payments
